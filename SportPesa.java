12ff67923efafbeb519ee10f7f58a3766612f126
GitauHarrison <harrisongitau3@gmail.com>
2016-09-29 15:13:23 +0300

    sport pesa toy problem

---
+++ SportPesa.java
@@ -0,0 +1,12 @@
+//Using java, come up with a small program which has an algorithm that
+//computes the bonus rewards for a sportpesa jackpot. Let your program pick
+// input from the user and then awards a cash bonus for 10, 11, and 12 correct
+//predictions. For 13 correct predictions, your algorithm should inform the user
+//that the jackpot has been won, and it should then generate a new value for the
+//jackpot.
+
+//Extra tips : Enjoy :-)
+
+public class sportpesa{
+
+}
